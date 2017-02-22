83a342e9d47c7ff92958a34320cce9761b42978a
Ani Sinanaj <ani.sinanaj@caffeina.com>
2017-02-22 09:35:39 +0100

    Finished textfield. Adding progressbar

---
+++ iphone/build/ti.material.build/Release-iphoneos/ti.material.build/Objects-normal/arm64/TiMaterialTextField.d
@@ -0,0 +1,38 @@
+dependencies: \
+  /Users/ani.sinanaj/Mobile/timaterial/ti.material/iphone/Classes/TiMaterialTextField.m \
+  /Users/ani.sinanaj/Mobile/timaterial/ti.material/iphone/headers/MDTextField.h \
+  /Users/ani.sinanaj/Mobile/timaterial/ti.material/iphone/Classes/TiMaterialTextField.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiUIView.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiProxy.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiEvaluator.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/KrollContext.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiToJS.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/TiCore.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/Ti.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/TiBase.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/TiContextRef.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/TiObjectRef.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/TiValueRef.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/WebKitAvailability.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/TiStringRef.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/JavaScriptCore/TiStringRefCF.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/KrollCallback.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/KrollObject.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiBindingRunLoop.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiBase.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/defines.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiThreading.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiPublicAPI.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiAnimation.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiPoint.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiDimension.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiColor.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/ListenerEntry.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiModule.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiUtils.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/WebFont.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiFile.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiBuffer.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiBlob.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/LayoutConstraint.h \
+  /Users/ani.sinanaj/Library/Application\ Support/Titanium/mobilesdk/osx/6.0.0.GA/iphone/include/TiGradient.h
