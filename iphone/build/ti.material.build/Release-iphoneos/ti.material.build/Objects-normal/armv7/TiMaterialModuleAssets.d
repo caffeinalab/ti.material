9216926ee83048bda1bfa1fe990641806d1fcd5b
Ani Sinanaj <ani.sinanaj@caffeina.com>
2017-02-24 16:52:14 +0100

    Removing build files

--- iphone/build/ti.material.build/Release-iphoneos/ti.material.build/Objects-normal/armv7/TiMaterialModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ani.sinanaj/Mobile/timaterial/ti.material/iphone/Classes/TiMaterialModuleAssets.m \
-  /Users/ani.sinanaj/Mobile/timaterial/ti.material/iphone/Classes/TiMaterialModuleAssets.h
